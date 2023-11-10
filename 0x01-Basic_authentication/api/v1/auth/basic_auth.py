0c407728a624463f68a01e7f67a23cbaaa421fb1
gideon <kipkerring.gedion@gmail.com>
2023-11-10 15:09:52 +0000

    basic_auth.py

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,8 +1,16 @@
 #!/usr/bin/env python3
 """a class BasicAuth that inherits from Auth"""
 from api.v1.auth.auth import Auth
+from base64 import b64decode, decode
+import base64
 
 
 class BasicAuth(Auth):
     """inherits from Auth"""
-    pass
+    def extract_base64_authorization_header(self, auth_header: str) -> str:
+        """returns the Base64 part of the Authorization header"""
+        if auth_header is None or not isinstance(auth_header, str):
+            return None
+        if 'Basic' not in auth_header:
+            return None
+        return auth_header[6:]
