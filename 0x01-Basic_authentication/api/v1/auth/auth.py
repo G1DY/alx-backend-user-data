9a4e0c59e341ff5ffe0c9890a010e990988f6504
gideon <kipkerring.gedion@gmail.com>
2023-11-10 10:21:54 +0000

    This class is the template for all authentication system you will implement.

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -8,7 +8,13 @@ class Auth:
     """manages API authentication"""
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """returns False - path and excluded_paths"""
-        return False
+        if path is None or excluded_paths is None or not excluded_paths:
+            return True
+        # make sure all paths end with a slash for comparison
+        path = path.rstrip("/") + "/"
+        excluded_paths = [p.rstrip("/") + "/" for p in excluded_paths]
+
+        return path not in excluded_paths
 
     def authorization_header(self, request=None) -> str:
         """returns None - request will be the Flask request object"""
