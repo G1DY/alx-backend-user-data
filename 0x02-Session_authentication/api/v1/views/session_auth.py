73840f8307dbd57c5d37b8bc6aad791719ff3e75
gideon <kipkerring.gedion@gmail.com>
2023-11-11 22:58:26 +0000

    session_auth.py

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -34,3 +34,15 @@ def session_login() -> str:
 
     user_dict.set_cookie(cookie_response, session_id)
     return user_dict
+
+
+@app_views.route(
+    '/auth_session/logout',
+    method=['DELETE'],
+    strict_slashes)
+def session_logout() -> str:
+    """DELETE /api/v1/auth_session/logout"""
+    from api.v1.app import auth
+    if auth.destroy_session(request):
+        return jsonify({}), 200
+    abort(404)
