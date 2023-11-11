0d3a213864fbd7ce007579b2aaa071c9c6ce9711
gideon <kipkerring.gedion@gmail.com>
2023-11-11 13:40:26 +0000

    instance method def create_session(self, user_id: str = None) -> str: that creates a Session ID for a user_id:

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,8 +1,24 @@
 #!/usr/bin/env python3
 """module with class SessionAuth"""
 from api.v1.auth.auth import Auth
+from uuid import uuid4
 
 
 class SessionAuth(Auth):
     """class SessionAuth that inherits from Auth"""
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """creates a Session ID for a user_id
+        Returns:
+          -Return None if user_id is None
+          -Return None if user_id is not a string
+          -Generate a Session ID using uuid module and uuid4() like id in Base
+          -Use this Session ID as key of the dictionary user_id_by_session_id
+          -Return the Session ID
+        """
+        if user_id is None or not isinstance(user_id, str):
+            return None
+        session_id = str(uuid.uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
