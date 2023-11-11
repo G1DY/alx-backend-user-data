3f023bf953cac5dc7c20ac1b9d4aed76d8fe9240
gideon <kipkerring.gedion@gmail.com>
2023-11-11 16:23:55 +0000

    app.py

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -56,10 +56,10 @@ def before_request() -> str:
         '/api/v1/auth_session/login/']
     if auth:
         if auth.require_auth(request.path, path_list):
-            request.current_user = auth.current_user(request)
             if auth.authorization_header(
                     request) is None and auth.session_cookie(request) is None:
                 abort(401)
+            request.current_user = auth.current_user(request)
             if auth.authorization_header(request) is None:
                 abort(401)
             if auth.current_user(request) is None:
