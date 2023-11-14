6fce030b0d4151b2755ce7760219f73314026f87
gideon <kipkerring.gedion@gmail.com>
2023-11-14 10:54:08 +0000

    registers user

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -2,9 +2,27 @@
 """hash password module
 """
 import bcrypt
+from db import DB
 
 
 def _hash_password(password: str) -> str:
     """takes in a password string arguments and returns bytes
     """
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """registers user_email and user_password under object User"""
+        try:
+            self._db.find_user_by(email=email)
+            raise ValueError("User {} already exists.".format(email))
+        except NoResultFound:
+            hashed_password = _hash_password(password)
+            return new_user
