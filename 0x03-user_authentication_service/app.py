30a4c63aad04a428682d07e1a39765632542a845
gideon <kipkerring.gedion@gmail.com>
2023-11-14 14:40:31 +0000

    app.py

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -50,7 +50,7 @@ def logout():
     """contain the session ID as a cookie with key session_id
         -return 403 if user does not exist
     """
-    session_id = request.cookies.get("session_id")
+    session_id = request.cookies.get("session_id", None)
     user = AUTH.get_user_from_session_id(session_id)
     if session_id is None or user is None:
         abort(403)
