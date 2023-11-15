d2877d767a2c8a7189cf2d518588b224cb6e1dba
gideon <kipkerring.gedion@gmail.com>
2023-11-15 06:41:37 +0000

    app.py

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -81,7 +81,7 @@ def get_reset_password_token() -> str:
     if email not in registered_emails:
         abort(403)
     reset_token = AUTH.get_reset_password_token(email)
-    return jsonify({"email": email, "reset_token": reset_token}), 200
+    return jsonify({"email": email, "reset_token": reset_token})
 
 
 if __name__ == "__main__":
