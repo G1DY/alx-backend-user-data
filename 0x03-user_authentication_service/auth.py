eede223063d52c39afd6f2f276d3ca39a2c3a1b3
gideon <kipkerring.gedion@gmail.com>
2023-11-15 07:08:49 +0000

    auth.py

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -86,7 +86,7 @@ class Auth:
         except NoResultFound:
             raise ValueError
 
-    def update_password(self, reset_token: str) -> None:
+    def update_password(self, reset_token: str, password: str) -> None:
         """finds user and updates the password"""
         if reset_token is None or password is None:
             return None
