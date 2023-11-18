0283da8b628b478b46d07780d750185d83b457ac
gideon <kipkerring.gedion@gmail.com>
2023-11-18 15:10:44 +0000

    encrypts password

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -2,6 +2,9 @@
 """Implements a hash_password function that expects one string
    argument name password and returns a salted
 """
+import bcrypt
+
+
 def hash_password(password: str) -> bytes:
     """hashing function"""
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
