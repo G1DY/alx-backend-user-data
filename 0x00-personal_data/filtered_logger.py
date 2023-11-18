a0f7e8c3b48ca92d7297ea389c520a2e2862ea88
gideon <kipkerring.gedion@gmail.com>
2023-11-18 13:40:43 +0000

    filtered_logger.py

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -46,7 +46,7 @@ def get_logger() -> logging.Logger:
     logger.propagate = False
 
     formatter = RedactingFormatter(list(PII_FIELDS))
-    target_handle.setFormatter(formatter)
+    stream_handler.setFormatter(formatter)
 
     logger.addHandler(stream_handler)
     return logger
